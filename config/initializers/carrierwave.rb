f4403b02bd10c38c17522a1788313aaf5f1dab76
Gai Margules <gai@monkeytech.co.il>
2014-08-04 13:06:16 +0300

    Initial commit_2

--- config/initializers/carrierwave.rb
+++
@@ -1,9 +0,0 @@
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-    provider: "AWS",
-    aws_access_key_id: Settings.aws_access_key_id,
-    aws_secret_access_key: Settings.aws_secret_access_key,
-    region: 'eu-west-1'
-  }
-  config.fog_directory = Settings.aws_s3_bucket
-end
--- config/initializers/session_store.rb
+++
@@ -1,3 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-Rails.application.config.session_store :cookie_store, key: '_zigdon_store_sample_session'
